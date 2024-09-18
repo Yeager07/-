dcfd692d62375aa4a2a9a43e7be909eeeaa72e22
Garek <Igor.Esipenko@urfu.me>
2024-09-18 14:55:40 +0500

    Workshop_3 файлы юнити

---
+++ Workshop_3_Ragdoll/Assets/ScriptRagdollOff.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRagdollOff : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody [] allRigidbodies;
+
+    private void Awake()
+    {
+        for (int i = 0; i < allRigidbodies.Length; i++)
+        {
+            allRigidbodies[i].isKinematic = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MakePhysics();
+        }
+    }
+
+    public void MakePhysics()
+    {
+        animator.enabled = false;
+        for (int i = 0; i < allRigidbodies.Length; i++)
+        {
+            allRigidbodies[i].isKinematic = false;
+        }
+    }
+}
